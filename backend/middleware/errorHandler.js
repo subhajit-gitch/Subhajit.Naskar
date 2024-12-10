7b2f8fec22b5f402bd4817ebd003a7f14b2d2cd7
Subhajit Naskar <133232117+subhajit-gitch@users.noreply.github.com>
2024-12-10 15:55:46 +0530

    Delete backend directory

--- backend/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
-};
-
-module.exports = errorHandler;
-console.log("error handelers is ready to use");
